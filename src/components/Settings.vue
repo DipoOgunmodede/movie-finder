f0af5e4a05d9e1195489c695dbfdefa909e46f00
Dipo Ogunmodede <dipoogunmodede@gmail.com>
2023-08-23 20:09:39 +0100

    fixed details element not opening bug

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,53 +1,51 @@
 <template>
-    <details id="settings" open="loadingOffsetValue !== 0">
+    <details id="settings" :open="loadingOffsetValue !== 0">
         <summary>Visual settings</summary>
         <label class="text-xl flex flex-col">
             Show loading screen for {{ loadingOffset }} additional milliseconds
-            <input type="range" v-model="loadingOffset" @input="handleSliderInput" min="0" max="10000" step="50" class="w-full md:w-1/2" />
+            <input type="range" v-model="loadingOffset" min="0" max="10000" step="50" class="w-full md:w-1/2" />
         </label>
     </details>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 
 export default {
-    props: {
-        loadingOffsetValue: {
-            type: Number,
-            default: 0
-        }
-    },
+    props: ['loadingOffsetValue'], // Receive the loadingOffsetValue from the parent component
     
     setup(props, { emit }) {
         const loadingOffset = ref(props.loadingOffsetValue)
-        let inputTimeout
 
         const saveLoadingOffsetToLocalStorage = () => {
             localStorage.setItem('loadingOffset', loadingOffset.value)
         }
 
         const getLoadingOffsetFromLocalStorage = () => {
-            return parseInt(localStorage.getItem('loadingOffset')) || 0
+            return parseInt(localStorage.getItem('loadingOffset'))
+        }
+        
+        const debouncedSave = () => {
+            clearTimeout(debounceTimeout)
+            debounceTimeout = setTimeout(saveLoadingOffsetToLocalStorage, 300)
         }
 
-        const handleSliderInput = () => {
-            clearTimeout(inputTimeout)
-            inputTimeout = setTimeout(() => {
-                saveLoadingOffsetToLocalStorage()
-                emit('update:loadingOffsetValue', loadingOffset.value)
-            }, 300)
-        }
+        watch(loadingOffset, () => {
+            saveLoadingOffsetToLocalStorage()
+            // Emit the updated loadingOffset value to the parent component
+            emit('update:loadingOffsetValue', loadingOffset.value)
+        })
+
+        loadingOffset.value = getLoadingOffsetFromLocalStorage()
 
         onMounted(() => {
-            if (loadingOffset.value !== 0) {
+            if (loadingOffset.value != 0) {
                 document.getElementById('settings').open = true
             }
         })
 
         return {
-            loadingOffset,
-            handleSliderInput
+            loadingOffset
         }
     }
 }

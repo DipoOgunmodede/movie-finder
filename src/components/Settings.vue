0c4e8b4d3477d16fb5fafe89022e044387dc934b
Dipo Ogunmodede <dipoogunmodede@gmail.com>
2023-08-23 19:43:07 +0100

    completed settings component

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,53 +1,42 @@
 <script>
 import { ref, watch, onMounted } from 'vue'
-const loadingOffset = ref(0)
-
-const saveLoadingOffsetToLocalStorage = () => {
-    localStorage.setItem('loadingOffset', loadingOffset.value)
-}
-const getloadingOffsetFromLocalStorage = () => { localStorage.getItem('loadingOffset') }
-
-//watch for changes to the loadingOffset value and save them to localstorage 
-watch(loadingOffset, () => {
-    saveLoadingOffsetToLocalStorage()
-}, { immediate: true })
-
-const checkLocalStorageForSettings = () => {
-    if (getloadingOffsetFromLocalStorage() != null) {
-        loadingOffset.value = getloadingOffsetFromLocalStorage()
-        console.log(`I am being fired by the checkLocalStorageForSettings function and the value of loadingOffset is ${loadingOffset.value}`)
-    }
-}
-
-const openSettingsCheck = () => {
-    //if localstorage values are anything other than the default values, open the details element by default
-    const storedLoadingOffset = localStorage.getItem('loadingOffset')
-    if (storedLoadingOffset !== 0) {
-        loadingOffset.value = storedLoadingOffset
-        document.getElementById('settings').open = true
-        console.log("I have been checked and the settings details element has been opened")
-    }
-}
-
-onMounted(loadingOffset, () => {
-    checkLocalStorageForSettings()
-    openSettingsCheck()
-})
-
-watch(loadingOffset, () => {
-    saveLoadingOffsetToLocalStorage()
-}, { immediate: true })
-
 
 export default {
     name: 'Settings',
     setup() {
+        const loadingOffset = ref(0)
+
+        const saveLoadingOffsetToLocalStorage = () => {
+            localStorage.setItem('loadingOffset', loadingOffset.value)
+        }
+
+        // Get the loadingOffset value from local storage
+        const getLoadingOffsetFromLocalStorage = () => {
+            return parseInt(localStorage.getItem('loadingOffset') || 0)
+        }
+
+        // Watch for changes to the loadingOffset value and save them to local storage
+        watch(loadingOffset, () => {
+            saveLoadingOffsetToLocalStorage()
+        })
+
+        // Set the initial value of loadingOffset based on local storage
+        loadingOffset.value = getLoadingOffsetFromLocalStorage()
+
+        // Open the settings details element if loadingOffset is not 0
+        onMounted(() => {
+            if (loadingOffset.value !== 0) {
+                document.getElementById('settings').open = true
+            }
+        })
+
         return {
             loadingOffset
         }
     }
 }
 </script>
+
 <template>
     <details id="settings">
         <summary>Visual settings</summary>
@@ -55,4 +44,4 @@ export default {
             <input type="range" v-model="loadingOffset" min="0" max="10000" step="50" class="w-full md:w-1/2" />
         </label>
     </details>
-</template> 
+</template>
